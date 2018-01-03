f1fa0e920c30430c7feb998504637f35cfb16566
Amer Aref <a.aref@studenti.gobettivolta.gov.com>
2018-01-03 14:56:26 +0100

    merge 3

---
+++ Meteo.UI/ChoseConfigurationPc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Meteo.UI
+{
+    public class ChoseConfigurationPc
+    {
+        public string ConfigAmer()
+        {
+            var path = "/Users/air/Documents/Progetti/Meteo/Meteo.Services/Connection.To.Database/DatabaseConnection.json";
+            return path;
+
+        }
+        public string ConfigGabriel()
+        {
+            var path = "/home/gabriel/Scrivania/Progetti/Meteo-Creazione-file/Meteo.Services/Connection.To.Database/DatabaseConnection.json";
+            return path;
+        }
+    }
+}
